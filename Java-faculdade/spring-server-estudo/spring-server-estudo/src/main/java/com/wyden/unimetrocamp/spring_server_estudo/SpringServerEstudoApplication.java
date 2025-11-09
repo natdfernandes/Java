7fd96971a6c44f94e7adbb0b744b873d12fb1bcd
Natalia <nataliadiasfernande@gmail.com>
2025-11-09 14:11:33 -0300

    Estudando spring boot

---
+++ Java-faculdade/spring-server-estudo/spring-server-estudo/src/main/java/com/wyden/unimetrocamp/spring_server_estudo/SpringServerEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.wyden.unimetrocamp.spring_server_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerEstudoApplication.class, args);
+	}
+
+}
---
+++ Java-faculdade/spring-server-estudo/spring-server-estudo/src/test/java/com/wyden/unimetrocamp/spring_server_estudo/SpringServerEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyden.unimetrocamp.spring_server_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
